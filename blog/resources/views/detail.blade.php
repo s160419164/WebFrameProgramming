3ede09830e81344938b2aee95b8e2301519d4a9c
Bayu <s160419164.student.ubaya.ac.id>
2022-02-23 18:18:08 +0700

    finishing

--- blog/resources/views/detail.blade.php
+++ blog/resources/views/detail.blade.php
@@ -87,47 +87,41 @@
                         </div>
                         <div>
                             <img src="{{asset('img/paracetamol.jpeg')}}"/>
-                        </div>
-                    </div>
-                    <div><h4>Paracetamol adalah obat untuk meredakan demam dan nyeri, termasuk nyeri haid atau sakit gigi. 
+                            <h4>Paracetamol adalah obat untuk meredakan demam dan nyeri, termasuk nyeri haid atau sakit gigi. 
                         Paracetamol atau acetaminophen tersedia dalam bentuk tablet, sirop, tetes, suppositoria, dan infus.</h4>
+                        </div>
                     </div>
                 @elseif($detailid=='medicine_2')
                         <div>
                             <div><h2><a href="{{route('detailmedicines',['id'=>'medicine_2'])}}">Promag</a></h4></div>
-                            <div><img src="{{asset('img/promag.jpg')}}"/></div>
-                       </div>
-                       <div><h4>Promag bermanfaat untuk mengatasi sakit maag, penyakit asam lambung naik, dan perut kembung. 
+                            <div><img src="{{asset('img/promag.jpg')}}"/><h4>Promag bermanfaat untuk mengatasi sakit maag, penyakit asam lambung naik, dan perut kembung. 
                            Obat ini dijual secara bebas. Promag tersedia dalam bentuk tablet kunyah dan suspensi cair dengan kandungan bahan 
                            aktif yang bervariasi di tiap jenisnya.</h4></div>
+                       </div>
                 @elseif($detailid=='medicine_3')
                         <div>
                             <div><h2><a href="{{route('detailmedicines',['id'=>'medicine_3'])}}">Konidin</a></h4></div>
-                            <div><img src="{{asset('img/konidin.jpg')}}"/></div>
-                       </div>
-                       <div><h4>Konidin bermanfaat untuk mengobati batuk berdahak, kering, batuk yang disebabkan oleh alergi hingga 
+                            <div><img src="{{asset('img/konidin.jpg')}}"/><h4>Konidin bermanfaat untuk mengobati batuk berdahak, kering, batuk yang disebabkan oleh alergi hingga 
                            menuntaskan sisa-sisa bronkitis.</h4></div>
+                       </div>
+
                 @elseif($detailid=='equip_1')
                          <div>
                             <div><h2><a href="{{route('detailequip',['id'=>'equip_1'])}}">Stetoskop</a></h4></div>
-                            <div><img src="{{asset('img/stetoskop.jpg')}}"/></div>
-                        </div>
-                        <div><h4>Alat ini berfungsi untuk mendengarkan suara yang terdapat di dalam tubuh. Seperti suara paru-paru, 
+                            <div><img src="{{asset('img/stetoskop.jpg')}}"/><h4>Alat ini berfungsi untuk mendengarkan suara yang terdapat di dalam tubuh. Seperti suara paru-paru, 
                             detak jantung, dan untuk memeriksa gangguan pada usus dan lambung.</h4></div>
                         </div>
                 @elseif($detailid=='equip_2')
                         <div>
                            <div><h2><a href="{{route('detailequip',['id'=>'equip_2'])}}">Tensimeter</a></h4></div>
-                           <div><img src="{{asset('img/tensimeter.jpg')}}"/></div>
-                           <div><h4>Tensimeter atau sfigmomanometer merupakan alat yang digunakan untuk mengukur tekanan darah</h4></div>
-                        </div>
-                @endif
+                           <div><img src="{{asset('img/tensimeter.jpg')}}"/><h4>Tensimeter atau sfigmomanometer merupakan alat yang digunakan untuk mengukur tekanan darah</h4></div>
+                        </div><br>
                 @elseif($detailid=='equip_3')
                         <div>
                            <div><h2><a href="{{route('detailequip',['id'=>'equip_3'])}}">Penlight</a></h4></div>
-                           <div><img src="{{asset('img/penlight.jpg')}}"/></div>
-                           <div><h4>Penlight merupakan intrumen kedokteran yang berguna untuk memeriksa di daerah gelap seperti rongga mulut, telinga, dll.</h4></div>
-                        </div>
+                           <div><img src="{{asset('img/penlight.jpg')}}"/>
+                           <h4>Penlight merupakan intrumen kedokteran yang berguna untuk memeriksa di daerah gelap seperti rongga mulut, telinga, dll.</h4></div>
+                        </div><br>
                 @endif
             </div>
         </div>
